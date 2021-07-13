f53f6de14cd2cc627b21b8e0e225a2f9dc836ed7
TO/GA <chenyi2000@zju.edu.cn>
2021-07-13 14:40:31 +0800

    use stduuid to generate image name

--- src/model/RenderModel.cc
+++ src/model/RenderModel.cc
@@ -1,6 +1,19 @@
 #include "RenderModel.hpp"
 
-RenderModel::RenderModel() {}
+#include <random>
+
+#include "uuid.h"
+
+RenderModel::RenderModel() {
+  std::random_device rd;
+  auto seed_data = std::array<int, std::mt19937::state_size>{};
+  std::generate(std::begin(seed_data), std::end(seed_data), std::ref(rd));
+  std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+  std::mt19937 generator(seq);
+  uuids::uuid_random_generator gen{generator};
+
+  image_name_ = uuids::to_string(gen()) + ".bmp";
+}
 
 RenderModel::~RenderModel() {}
 
