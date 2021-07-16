6b2710d0f0fb8977a8efd8d6e995d450a569131a
zlz <3190101094@zju.edu.cn>
2021-07-16 20:33:25 +0800

    add a progress bar

---
+++ src/view/ProgressBar.cc
@@ -0,0 +1,42 @@
+
+#include <memory>
+#include <cstring>
+#include "precomp.hpp"
+#include "ProgressBar.h"
+
+ProgressBar::ProgressBar(int x, int y, int w, int h)noexcept
+    : Fl_Box(x, y, w, h),
+    last_progress_(0), 
+    buf_(new unsigned char[(int64_t)w*h*3]),
+    img_((const unsigned char*)(buf_.get()),w,h) 
+{
+  memset(buf_.get(), 119, 3*(int64_t)w * h*sizeof(unsigned char));
+  image(img_);
+}
+
+ProgressBar::~ProgressBar()noexcept {}
+
+void ProgressBar::show()noexcept { 
+  int i, j;
+  int progress = *(progress_.Get());
+  unsigned char pallet[]{24, 237, 51};
+  if (progress >100 || progress < last_progress_) return;
+  unsigned char* buf = buf_.get();
+  for (i = 0; i < h(); i++) {
+    for (j = last_progress_*w()/100+i*w(); j < progress*w()/100+i*w(); j++) {
+      buf[3*j] = pallet[0];
+      buf[3*j + 1] = pallet[1];
+      buf[3*j + 2] = pallet[2];
+    }
+  }
+  last_progress_ = progress;
+  img_.uncache();
+  image(img_);
+  redraw();
+}
+
+void ProgressBar::attach_progress(CSL::RefPtr<int>& pg) noexcept {
+  progress_ = pg;
+}
+
+CSL::RefPtr<int> ProgressBar::detach_progress() noexcept { return progress_; }
---
+++ src/view/ProgressBar.h
@@ -0,0 +1,24 @@
+
+#ifndef __PROGRESS_BAR_H__
+#define __PROGRESS_BAR_H__
+
+class ProgressBar : public Fl_Box {
+ public:
+  ProgressBar(int x, int y, int w, int h)noexcept;
+  ~ProgressBar()noexcept;
+
+  // property
+  void attach_progress(CSL::RefPtr<int>& pg)noexcept;
+  CSL::RefPtr<int> detach_progress()noexcept;
+  // method
+  void show()noexcept;
+
+ private:
+  std::unique_ptr<unsigned char> buf_;
+  CSL::RefPtr<int> progress_;
+  int last_progress_;
+  Fl_RGB_Image img_;
+};
+
+#endif  // !__PROGRESS_BAR_H__
+
