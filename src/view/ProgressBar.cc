1320de1a3c9825063801be78013967ff50b14013
zlz <3190101094@zju.edu.cn>
2021-07-17 11:42:11 +0800

    fix progress bar

--- src/view/ProgressBar.cc
+++ src/view/ProgressBar.cc
@@ -10,7 +10,16 @@ ProgressBar::ProgressBar(int x, int y, int w, int h)noexcept
     buf_(new unsigned char[(int64_t)w*h*3]),
     img_((const unsigned char*)(buf_.get()),w,h) 
 {
-  memset(buf_.get(), 119, 3*(int64_t)w * h*sizeof(unsigned char));
+  unsigned char pallet[]{119,119,119};
+  unsigned char* buf = buf_.get();
+  int i,j;
+  for (i = 0; i < h; i++) {
+    for (j = 0;j<w; j++) {
+      buf[3*j] = pallet[0];
+      buf[3*j + 1] = pallet[1];
+      buf[3*j + 2] = pallet[2];
+    }
+  }
   image(img_);
 }
 
@@ -20,15 +29,29 @@ void ProgressBar::show()noexcept {
   int i, j;
   int progress = *(progress_.Get());
   unsigned char pallet[]{24, 237, 51};
-  if (progress >100 || progress < last_progress_) return;
+  unsigned char cancel_pallet[]{119,119,119};
+  if (progress >100 || progress < 0) return;
   unsigned char* buf = buf_.get();
-  for (i = 0; i < h(); i++) {
-    for (j = last_progress_*w()/100+i*w(); j < progress*w()/100+i*w(); j++) {
-      buf[3*j] = pallet[0];
-      buf[3*j + 1] = pallet[1];
-      buf[3*j + 2] = pallet[2];
+
+  if(progress>=last_progress_){
+    for (i = 0; i < h(); i++) {
+      for (j = last_progress_*w()/100+i*w(); j < progress*w()/100+i*w(); j++) {
+        buf[3*j] = pallet[0];
+        buf[3*j + 1] = pallet[1];
+        buf[3*j + 2] = pallet[2];
+      }
     }
   }
+  else{
+    for (i = 0; i < h(); i++) {
+      for (j = progress*w()/100+i*w(); j < last_progress_*w()/100+i*w(); j++) {
+        buf[3*j] = cancel_pallet[0];
+        buf[3*j + 1] = cancel_pallet[1];
+        buf[3*j + 2] = cancel_pallet[2];
+      }
+    }    
+  }
+
   last_progress_ = progress;
   img_.uncache();
   redraw();
