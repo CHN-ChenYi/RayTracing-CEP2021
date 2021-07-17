66b1eac4edeb0eab6953b93083bcc5a0146c7a70
zlz <3190101094@zju.edu.cn>
2021-07-17 14:50:07 +0800

    add text model and text view model

---
+++ src/model/TextModel.h
@@ -0,0 +1,24 @@
+
+#ifndef __TEXT_MODEL_H__
+#define __TEXT_MODEL_H__
+
+class TextModel : public PropertyTrigger
+{
+public:
+	TextModel();
+	TextModel(const TextModel&) = delete;
+	TextModel& operator=(const TextModel&) = delete;
+	~TextModel() noexcept;
+
+//properties
+	RefPtr<Fl_Text_Buffer> get_TextBuf() noexcept;
+
+//methods
+	bool Load(const std::string& str);
+	bool Save(const std::string& str);
+
+private:
+	Fl_Text_Buffer  m_textBuffer;
+};
+
+#endif
